2f93bde9a6a11d37741a4aafaff09683e115eb0c
dvub <dvubdevs@gmail.com>
2023-11-23 23:39:52 -0800

    Add comments, refactor, implementation almost done

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ struct Args {
     /// The directory to list files under
     path: String,
 
-    /// Print directories and sub-directories without printing any contained files
+    /// Print only directories without printing any contained files
     #[arg(short, long)]
     dirs: bool,
 
@@ -17,47 +17,67 @@ struct Args {
 }
 fn main() {
     let args = Args::parse();
-
     ls(Path::new(&args.path), args.dirs, 0, args.max_depth);
 }
 
 fn ls(path: &Path, dirs_only: bool, depth: usize, max_depth: Option<usize>) {
+    // base case checks pretty much
+    // if there's a max depth and we're above it, stop,
+    // if the path doesn't exist, stop here
+    // if the path is a file and we marked dirs_only then stop
+    if max_depth.map_or(false, |d| depth >= d)
+        || (!path.is_dir() && dirs_only)
+        || path.file_name().is_none()
+    {
+        return;
+    }
+    // this would go above but we should print a helpful message instead of simply returning
     if !path.exists() {
+        println!("{}: directory doesn't exist. Exiting...", "Error".red());
         return;
     }
 
-    if !path.is_dir() && dirs_only {
-        return;
-    }
-
-    if path.file_name().is_none() {
-        return;
-    }
-
+    // here we'll actually print out the file name
     print_file(path, depth);
 
-    if path.is_dir() {
-        let result = read_dir(path).unwrap();
+    // todo.. check if path is a directory before read_dir() ...
+
+    if let Ok(result) = read_dir(path) {
         result.for_each(|dir| {
-            let entry = dir.unwrap().path();
-            ls(&entry, depth + 1, dirs_only);
+            if let Ok(entry) = dir {
+                // recursion
+                ls(&entry.path(), dirs_only, depth + 1, max_depth);
+            }
         });
     }
 }
 
 fn print_file(path: &Path, num_tabs: usize) {
-    let mut tabs: String = String::new();
-    for _ in 0..num_tabs {
-        tabs.push_str("\t");
+    // unwrapping a lot because we've already made all of these checks in the ls function
+    // so this should be ok
+    // as for to_str().unwrap(), idk.
+    let mut file: ColoredString = path.file_name().unwrap().to_str().unwrap().into();
+
+    if file.starts_with('.') {
+        return;
     }
 
-    let mut file: ColoredString = path.file_name().unwrap().to_str().unwrap().into();
+    // generate the number of indents/tabs before printing
+    let mut tabs: String = String::new();
+    for _ in 0..num_tabs {
+        tabs.push('\t');
+    }
+
+    // color based on file/dir
+    // todo: improve this ...?
     if path.is_dir() {
         file = file.red().bold();
     } else if path.is_file() {
         file = file.blue();
     }
-    println!("{} {}", tabs, file);
+    // print
+    println!("{}{}", tabs, file);
+    // possibly use these symbols or something idk
     // ∟⊢
 }
 
@@ -66,7 +86,6 @@ mod tests {
     const PARENT_TEST_DIR: &str = "test-dir";
     use std::{
         env,
-        error::Error,
         fs::{create_dir, read_dir, remove_dir_all},
         io,
     };
@@ -84,20 +103,21 @@ mod tests {
     // deletes everything in the parent test directory, returning a result
     fn teardown() -> Result<(), io::Error> {
         let target = env::current_dir().unwrap().join(PARENT_TEST_DIR);
-        remove_dir_all(&target)
+        remove_dir_all(target)
     }
 
     #[test]
     fn test_setup() {
         let target = env::current_dir().unwrap().join(PARENT_TEST_DIR);
         setup();
-        assert_eq!(read_dir(&target).unwrap().count(), 3);
+        assert_eq!(read_dir(target).unwrap().count(), 3);
+        teardown().unwrap();
     }
     #[test]
     fn test_teardown() {
         let target = env::current_dir().unwrap().join(PARENT_TEST_DIR);
         setup();
         assert!(teardown().is_ok());
-        assert!(read_dir(&target).is_err());
+        assert!(read_dir(target).is_err());
     }
 }
